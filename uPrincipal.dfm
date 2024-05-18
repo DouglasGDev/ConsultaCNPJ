c0865dbb469aea98ef0fa0705a88ec62240ff850
DouglasGDev <100414633+DouglasGDev@users.noreply.github.com>
2024-05-17 20:08:50 -0400

    melhorias

--- uPrincipal.dfm
+++ uPrincipal.dfm
@@ -1,10 +1,10 @@
 object frmPrincipal: TfrmPrincipal
   Left = 0
   Top = 0
-  BorderStyle = bsNone
+  BorderStyle = bsToolWindow
   Caption = 'CONSULTA'
-  ClientHeight = 619
-  ClientWidth = 1005
+  ClientHeight = 592
+  ClientWidth = 997
   Color = clBtnFace
   Font.Charset = DEFAULT_CHARSET
   Font.Color = clWindowText
@@ -12,47 +12,70 @@ object frmPrincipal: TfrmPrincipal
   Font.Name = 'Segoe UI'
   Font.Style = []
   Position = poScreenCenter
+  OnClose = FormClose
   TextHeight = 15
   object pnlHeader: TPanel
     Left = 0
     Top = 0
-    Width = 1005
+    Width = 997
     Height = 80
     Align = alTop
     BevelOuter = bvNone
     Color = clTeal
     ParentBackground = False
     TabOrder = 0
-    ExplicitWidth = 841
-    object edtBuscaCNPJ: TEdit
+    ExplicitTop = -6
+    object lblConsultaCNPJ: TLabel
       Left = 56
-      Top = 29
-      Width = 209
-      Height = 23
-      TabOrder = 0
-      TextHint = 'DIGITE O CNPJ(SOMENTE N'#218'MEROS)'
+      Top = 8
+      Width = 98
+      Height = 15
+      Caption = 'CONSULTAR CNPJ'
+      Font.Charset = DEFAULT_CHARSET
+      Font.Color = clWhite
+      Font.Height = -12
+      Font.Name = 'Segoe UI'
+      Font.Style = [fsBold]
+      ParentFont = False
     end
     object btnBuscarCNPJ: TButton
-      Left = 288
+      Left = 190
       Top = 29
       Width = 75
       Height = 23
       Caption = 'Buscar CNPJ'
-      TabOrder = 1
+      Font.Charset = DEFAULT_CHARSET
+      Font.Color = clWindowText
+      Font.Height = -12
+      Font.Name = 'Segoe UI'
+      Font.Style = [fsBold]
+      ParentFont = False
+      TabOrder = 0
       OnClick = btnBuscarCNPJClick
+    end
+    object edtBuscaCNPJ: TMaskEdit
+      Left = 56
+      Top = 29
+      Width = 121
+      Height = 23
+      EditMask = '00.000.000/0000-00;1;_'
+      MaxLength = 18
+      TabOrder = 1
+      Text = '  .   .   /    -  '
     end
   end
   object pnlFooter: TPanel
     Left = 0
-    Top = 539
-    Width = 1005
+    Top = 512
+    Width = 997
     Height = 80
     Align = alBottom
     BevelOuter = bvNone
     Color = clTeal
     ParentBackground = False
     TabOrder = 1
-    ExplicitWidth = 841
+    ExplicitTop = 539
+    ExplicitWidth = 1005
     object btnFechar: TBitBtn
       Left = 909
       Top = 28
@@ -66,15 +89,28 @@ object frmPrincipal: TfrmPrincipal
   object pnlBody: TPanel
     Left = 0
     Top = 80
-    Width = 1005
-    Height = 459
+    Width = 997
+    Height = 432
     Align = alClient
     BevelOuter = bvSpace
     Color = 16768959
     ParentBackground = False
     TabOrder = 2
-    ExplicitTop = 86
-    ExplicitWidth = 911
+    ExplicitWidth = 1005
+    ExplicitHeight = 459
+    object Label1: TLabel
+      Left = 534
+      Top = 5
+      Width = 27
+      Height = 15
+      Caption = 'CNPJ'
+      Font.Charset = DEFAULT_CHARSET
+      Font.Color = clWindowText
+      Font.Height = -12
+      Font.Name = 'Segoe UI'
+      Font.Style = []
+      ParentFont = False
+    end
     object edtRazaoSocial: TLabeledEdit
       Left = 8
       Top = 22
@@ -99,18 +135,6 @@ object frmPrincipal: TfrmPrincipal
       TabOrder = 1
       Text = ''
     end
-    object edtCNPJ: TLabeledEdit
-      Left = 534
-      Top = 22
-      Width = 139
-      Height = 23
-      EditLabel.Width = 27
-      EditLabel.Height = 15
-      EditLabel.Caption = 'CNPJ'
-      ReadOnly = True
-      TabOrder = 2
-      Text = ''
-    end
     object edtTipoLogradouro: TLabeledEdit
       Left = 8
       Top = 62
@@ -120,7 +144,7 @@ object frmPrincipal: TfrmPrincipal
       EditLabel.Height = 15
       EditLabel.Caption = 'TIPO LOGRADOURO'
       ReadOnly = True
-      TabOrder = 3
+      TabOrder = 2
       Text = ''
     end
     object edtLogradouro: TLabeledEdit
@@ -132,7 +156,7 @@ object frmPrincipal: TfrmPrincipal
       EditLabel.Height = 15
       EditLabel.Caption = 'LOGRADOURO'
       ReadOnly = True
-      TabOrder = 4
+      TabOrder = 3
       Text = ''
     end
     object edtNumero: TLabeledEdit
@@ -144,11 +168,11 @@ object frmPrincipal: TfrmPrincipal
       EditLabel.Height = 15
       EditLabel.Caption = 'NUMERO'
       ReadOnly = True
-      TabOrder = 5
+      TabOrder = 4
       Text = ''
     end
     object edtIE: TLabeledEdit
-      Left = 845
+      Left = 685
       Top = 22
       Width = 139
       Height = 23
@@ -156,19 +180,19 @@ object frmPrincipal: TfrmPrincipal
       EditLabel.Height = 15
       EditLabel.Caption = 'INSCRI'#199#195'O ESTADUAL'
       ReadOnly = True
-      TabOrder = 6
+      TabOrder = 5
       Text = ''
     end
     object edtIEAtivo: TLabeledEdit
-      Left = 845
-      Top = 62
-      Width = 73
+      Left = 830
+      Top = 22
+      Width = 139
       Height = 23
       EditLabel.Width = 45
       EditLabel.Height = 15
       EditLabel.Caption = 'I.E/Ativo'
       ReadOnly = True
-      TabOrder = 7
+      TabOrder = 6
       Text = ''
     end
     object edtComplemento: TLabeledEdit
@@ -180,7 +204,7 @@ object frmPrincipal: TfrmPrincipal
       EditLabel.Height = 15
       EditLabel.Caption = 'COMPLEMENTO'
       ReadOnly = True
-      TabOrder = 8
+      TabOrder = 7
       Text = ''
     end
     object edtBairro: TLabeledEdit
@@ -192,7 +216,7 @@ object frmPrincipal: TfrmPrincipal
       EditLabel.Height = 15
       EditLabel.Caption = 'BAIRRO'
       ReadOnly = True
-      TabOrder = 9
+      TabOrder = 8
       Text = ''
     end
     object edtCEP: TLabeledEdit
@@ -204,7 +228,7 @@ object frmPrincipal: TfrmPrincipal
       EditLabel.Height = 15
       EditLabel.Caption = 'CEP'
       ReadOnly = True
-      TabOrder = 10
+      TabOrder = 9
       Text = ''
     end
     object edtEstado: TLabeledEdit
@@ -216,7 +240,7 @@ object frmPrincipal: TfrmPrincipal
       EditLabel.Height = 15
       EditLabel.Caption = 'UF'
       ReadOnly = True
-      TabOrder = 11
+      TabOrder = 10
       Text = ''
     end
     object edtCidade: TLabeledEdit
@@ -228,7 +252,7 @@ object frmPrincipal: TfrmPrincipal
       EditLabel.Height = 15
       EditLabel.Caption = 'CIDADE'
       ReadOnly = True
-      TabOrder = 12
+      TabOrder = 11
       Text = ''
     end
     object edtTelefone: TLabeledEdit
@@ -240,7 +264,7 @@ object frmPrincipal: TfrmPrincipal
       EditLabel.Height = 15
       EditLabel.Caption = 'TELEFONE'
       ReadOnly = True
-      TabOrder = 13
+      TabOrder = 12
       Text = ''
     end
     object edtDDD: TLabeledEdit
@@ -252,7 +276,7 @@ object frmPrincipal: TfrmPrincipal
       EditLabel.Height = 15
       EditLabel.Caption = 'DDD'
       ReadOnly = True
-      TabOrder = 14
+      TabOrder = 13
       Text = ''
     end
     object edtDDD2: TLabeledEdit
@@ -264,7 +288,7 @@ object frmPrincipal: TfrmPrincipal
       EditLabel.Height = 15
       EditLabel.Caption = 'DDD2'
       ReadOnly = True
-      TabOrder = 15
+      TabOrder = 14
       Text = ''
     end
     object edtTelefone2: TLabeledEdit
@@ -276,7 +300,7 @@ object frmPrincipal: TfrmPrincipal
       EditLabel.Height = 15
       EditLabel.Caption = 'TELEFONE2'
       ReadOnly = True
-      TabOrder = 16
+      TabOrder = 15
       Text = ''
     end
     object edtEmail: TLabeledEdit
@@ -288,20 +312,42 @@ object frmPrincipal: TfrmPrincipal
       EditLabel.Height = 15
       EditLabel.Caption = 'EMAIL'
       ReadOnly = True
-      TabOrder = 17
+      TabOrder = 16
       Text = ''
     end
     object edtSituacaoCadastral: TLabeledEdit
-      Left = 684
-      Top = 22
+      Left = 174
+      Top = 102
       Width = 140
       Height = 23
       EditLabel.Width = 124
       EditLabel.Height = 15
       EditLabel.Caption = 'SITUA'#199#195'O CADASTRAL'
       ReadOnly = True
+      TabOrder = 17
+      Text = ''
+    end
+    object edtMotivo: TLabeledEdit
+      Left = 320
+      Top = 102
+      Width = 504
+      Height = 23
+      EditLabel.Width = 190
+      EditLabel.Height = 15
+      EditLabel.Caption = 'MOTIVO DA SITUA'#199#195'O CADASTRAL'
+      ReadOnly = True
       TabOrder = 18
       Text = ''
+    end
+    object edtCNPJ: TMaskEdit
+      Left = 534
+      Top = 22
+      Width = 139
+      Height = 23
+      EditMask = '00.000.000/0000-00;1;_'
+      MaxLength = 18
+      TabOrder = 19
+      Text = '  .   .   /    -  '
     end
   end
   object RESTRequest: TRESTRequest
--- uPrincipal.pas
+++ uPrincipal.pas
@@ -14,14 +14,12 @@ type
     pnlFooter: TPanel;
     pnlBody: TPanel;
     btnFechar: TBitBtn;
-    edtBuscaCNPJ: TEdit;
     btnBuscarCNPJ: TButton;
     RESTRequest: TRESTRequest;
     RESTResponse: TRESTResponse;
     RESTClient: TRESTClient;
     edtRazaoSocial: TLabeledEdit;
     edtFantasia: TLabeledEdit;
-    edtCNPJ: TLabeledEdit;
     edtTipoLogradouro: TLabeledEdit;
     edtLogradouro: TLabeledEdit;
     edtNumero: TLabeledEdit;
@@ -38,9 +36,16 @@ type
     edtTelefone2: TLabeledEdit;
     edtEmail: TLabeledEdit;
     edtSituacaoCadastral: TLabeledEdit;
+    lblConsultaCNPJ: TLabel;
+    edtMotivo: TLabeledEdit;
+    edtBuscaCNPJ: TMaskEdit;
+    edtCNPJ: TMaskEdit;
+    Label1: TLabel;
     procedure btnFecharClick(Sender: TObject);
     procedure btnBuscarCNPJClick(Sender: TObject);
+    procedure FormClose(Sender: TObject; var Action: TCloseAction);
   private
+    procedure LimparEdits;
     { Private declarations }
   public
     { Public declarations }
@@ -54,6 +59,48 @@ var
 implementation
 
 {$R *.dfm}
+procedure TfrmPrincipal.LimparEdits;
+var i: integer;
+begin
+  for i := 0 to ComponentCount -1 do begin
+    if (Components[i] is TLabeledEdit) then // verifica se é do tipo TLabeledEdit
+      TLabeledEdit(Components[i]).Text := EmptyStr// para limpar o campo que foi digitado anteriormente e retornar sem sujeira
+       else if (Components[i] is TMaskEdit) then
+       TMaskEdit(Components[i]).Text := EmptyStr// para limpar o campo que foi digitado anteriormente e retornar sem sujeira
+       else if (Components[i] is TEdit) then
+       TEdit(Components[i]).Text := EmptyStr// para limpar o campo que foi digitado anteriormente e retornar sem sujeira
+       else if (Components[i] is TMemo) then
+       TMemo(Components[i]).Text := EmptyStr// para limpar o campo que foi digitado anteriormente e retornar sem sujeira
+       //else if (Components[i] is TDBLookupComboBox) then
+       //TDBLookupComboBox(Components[i]).KeyValue := Null// para limpar o campo que foi digitado anteriormente e retornar sem sujeira
+       //else if (Components[i] is TCurrencyEdit) then
+       //TCurrencyEdit(Components[i]).Value := 0// para limpar o campo que foi digitado anteriormente e retornar sem sujeira
+       //else if (Components[i] is TDateEdit) then
+       //TDateEdit(Components[i]).Date := 0;// para limpar o campo que foi digitado anteriormente e retornar sem sujeira
+  end;
+end;
+
+function ApenasNumeros(const Texto: string): string;
+var
+  i: Integer;
+begin
+  Result := ''; // Inicializa o resultado como uma string vazia
+  for i := 1 to Length(Texto) do
+  begin
+    if CharInSet(Texto[i], ['0'..'9']) then
+      Result := Result + Texto[i];
+  end;
+end;
+
+function AplicarMascaraCNPJ(const CNPJ: string): string;
+begin
+  // Assume que o CNPJ já está apenas com números e tem 14 caracteres
+  if Length(CNPJ) = 14 then
+    Result := Format('%.2s.%.3s.%.3s/%.4s-%.2s', [Copy(CNPJ, 1, 2), Copy(CNPJ, 3, 3),
+      Copy(CNPJ, 6, 3), Copy(CNPJ, 9, 4), Copy(CNPJ, 13, 2)])
+  else
+    Result := CNPJ; // Retorna o CNPJ sem máscara se não tiver 14 caracteres
+end;
 
 procedure TfrmPrincipal.btnBuscarCNPJClick(Sender: TObject);
 var
@@ -62,37 +109,119 @@ var
   InscricaoEstadual: TJSONValue;
 begin
   edtBuscaCNPJ.SetFocus;
-  LCNPJ := Trim(edtBuscaCNPJ.Text);
+  LCNPJ := Trim(ApenasNumeros(edtBuscaCNPJ.Text));
   RESTClient.BaseURL := Format(_URL_CONSULTAR_CNPJ, [LCNPJ]);
   RESTClient.SecureProtocols := [THTTPSecureProtocol.TLS12];
   RESTRequest.Method := rmGet;
 
   try
     try
+      LimparEdits;
       RESTRequest.Execute;
       case RESTResponse.StatusCode of
         200: begin
                JSONObject := TJSONObject.ParseJSONValue(TEncoding.UTF8.GetBytes(RESTResponse.Content), 0) as TJSONObject;
                  try
-                   edtRazaoSocial.Text :=  JSONObject.FindValue('razao_social').Value;
-                   edtFantasia.Text :=  JSONObject.FindValue('estabelecimento.nome_fantasia').Value;
-                   edtCNPJ.Text :=  JSONObject.FindValue('estabelecimento.cnpj').Value;
-                   edtTipoLogradouro.Text :=  JSONObject.FindValue('estabelecimento.tipo_logradouro').Value;
-                   edtLogradouro.Text :=  JSONObject.FindValue('estabelecimento.logradouro').Value;
-                   edtNumero.Text :=  JSONObject.FindValue('estabelecimento.numero').Value;
-                   edtBairro.Text :=  JSONObject.FindValue('estabelecimento.bairro').Value;
-                   edtCEP.Text := JSONObject.FindValue('estabelecimento.cep').Value;
-                   edtDDD.Text := JSONObject.FindValue('estabelecimento.ddd1').Value;
-                   edtTelefone.Text := JSONObject.FindValue('estabelecimento.telefone1').Value;
-                   edtDDD2.Text := JSONObject.FindValue('estabelecimento.ddd2').Value;
-                   edtTelefone2.Text := JSONObject.FindValue('estabelecimento.telefone2').Value;
-                   edtEmail.Text := JSONObject.FindValue('estabelecimento.email').Value;
-                   edtEstado.Text :=  JSONObject.FindValue('estabelecimento.estado.sigla').Value;
-                   edtCidade.Text :=  JSONObject.FindValue('estabelecimento.cidade.nome').Value;
-                   edtComplemento.Text :=  JSONObject.FindValue('estabelecimento.complemento').Value;
-                   edtSituacaoCadastral.Text :=  JSONObject.FindValue('estabelecimento.situacao_cadastral').Value;
-                   edtIE.Text := JSONObject.FindValue('estabelecimento.inscricoes_estaduais[0].inscricao_estadual').Value;
-                   edtIEAtivo.Text :=  JSONObject.FindValue('estabelecimento.inscricoes_estaduais[0].ativo').Value;
+                     if JSONObject.FindValue('razao_social') <> nil then
+                       edtRazaoSocial.Text :=  JSONObject.FindValue('razao_social').Value
+                     else
+                     edtRazaoSocial.Text :='';
+
+                     if JSONObject.FindValue('estabelecimento.nome_fantasia') <> nil then
+                      edtFantasia.Text :=  JSONObject.FindValue('estabelecimento.nome_fantasia').Value
+                     else
+                      edtFantasia.Text := '';
+
+                     if JSONObject.FindValue('estabelecimento.cnpj') <> nil then
+                       edtCNPJ.Text :=  AplicarMascaraCNPJ(JSONObject.FindValue('estabelecimento.cnpj').Value)
+                     else
+                       edtCNPJ.Text := '';
+
+                     if JSONObject.FindValue('estabelecimento.tipo_logradouro') <> nil then
+                       edtTipoLogradouro.Text :=  JSONObject.FindValue('estabelecimento.tipo_logradouro').Value
+                     else
+                       edtTipoLogradouro.Text := '';
+
+                     if JSONObject.FindValue('estabelecimento.logradouro') <> nil then
+                       edtLogradouro.Text :=  JSONObject.FindValue('estabelecimento.logradouro').Value
+                     else
+                       edtLogradouro.Text := '';
+
+                     if JSONObject.FindValue('estabelecimento.numero') <> nil then
+                       edtNumero.Text :=  JSONObject.FindValue('estabelecimento.numero').Value
+                     else
+                       edtNumero.Text := '';
+
+                     if JSONObject.FindValue('estabelecimento.bairro') <> nil then
+                       edtBairro.Text :=  JSONObject.FindValue('estabelecimento.bairro').Value
+                     else
+                       edtBairro.Text := '';
+
+                     if JSONObject.FindValue('estabelecimento.cep') <> nil then
+                       edtCEP.Text := JSONObject.FindValue('estabelecimento.cep').Value
+                     else
+                       edtCEP.Text := '';
+
+                      if JSONObject.FindValue('estabelecimento.ddd1') <> nil then
+                        edtDDD.Text := JSONObject.FindValue('estabelecimento.ddd1').Value
+                      else
+                        edtDDD.Text := '';
+
+                      if JSONObject.FindValue('estabelecimento.telefone1') <> nil then
+                        edtTelefone.Text := JSONObject.FindValue('estabelecimento.telefone1').Value
+                      else
+                        edtTelefone.Text := '';
+
+                      if JSONObject.FindValue('estabelecimento.ddd2') <> nil then
+                        edtDDD2.Text := JSONObject.FindValue('estabelecimento.ddd2').Value
+                      else
+                        edtDDD2.Text := '';
+
+                      if JSONObject.FindValue('estabelecimento.telefone2') <> nil then
+                        edtTelefone2.Text := JSONObject.FindValue('estabelecimento.telefone2').Value
+                      else
+                        edtTelefone2.Text := '';
+
+                      if JSONObject.FindValue('estabelecimento.email') <> nil then
+                        edtEmail.Text := JSONObject.FindValue('estabelecimento.email').Value
+                      else
+                        edtEmail.Text := '';
+
+                      if JSONObject.FindValue('estabelecimento.estado.sigla') <> nil then
+                        edtEstado.Text :=  JSONObject.FindValue('estabelecimento.estado.sigla').Value
+                      else
+                        edtEstado.Text := '';
+
+                      if JSONObject.FindValue('estabelecimento.cidade.nome') <> nil then
+                        edtCidade.Text :=  JSONObject.FindValue('estabelecimento.cidade.nome').Value
+                      else
+                        edtCidade.Text := '';
+
+                      if JSONObject.FindValue('estabelecimento.complemento') <> nil then
+                        edtComplemento.Text :=  JSONObject.FindValue('estabelecimento.complemento').Value
+                      else
+                        edtComplemento.Text := '';
+
+                      if JSONObject.FindValue('estabelecimento.situacao_cadastral') <> nil then
+                        edtSituacaoCadastral.Text :=  JSONObject.FindValue('estabelecimento.situacao_cadastral').Value
+                      else
+                        edtSituacaoCadastral.Text := '';
+
+                      if JSONObject.FindValue('estabelecimento.inscricoes_estaduais[0].inscricao_estadual') <> nil then
+                        edtIE.Text := JSONObject.FindValue('estabelecimento.inscricoes_estaduais[0].inscricao_estadual').Value
+                      else
+                        edtIE.Text := 'ISENTO';
+
+                      if JSONObject.FindValue('estabelecimento.inscricoes_estaduais[0].ativo') <> nil then
+                        edtIEAtivo.Text :=  JSONObject.FindValue('estabelecimento.inscricoes_estaduais[0].ativo').Value
+                      else
+                        edtIEAtivo.Text := 'INATIVO';
+
+                      if JSONObject.FindValue('estabelecimento.motivo_situacao_cadastral.descricao') <> nil then
+                        edtMotivo.Text := JSONObject.FindValue('estabelecimento.motivo_situacao_cadastral.descricao').Value
+                      else
+                        edtMotivo.Text := '';
+
                  finally
                  JSONObject.Free;
                end;
@@ -116,6 +245,11 @@ end;
 procedure TfrmPrincipal.btnFecharClick(Sender: TObject);
 begin
   Close;
+end;
+
+procedure TfrmPrincipal.FormClose(Sender: TObject; var Action: TCloseAction);
+begin
+   Application.Terminate;
 end;
 
 end.
